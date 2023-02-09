2ce9d371325b53e562396c9f12f582c90a19dddc
BeratK2 <bales1801@gmail.com>
2023-02-09 10:38:12 -0500

    Fixed chat being slow and fixed UI

--- flowchat/src/pages/Chat.jsx
+++ flowchat/src/pages/Chat.jsx
@@ -1,33 +1,35 @@
-import AgoraRTM from "agora-rtm-sdk";
-import { v4 as uuid } from "uuid";
-import { useEffect } from "react";
-import "./Styling/Chat.css";
-import { useState } from "react";
+import AgoraRTM from 'agora-rtm-sdk';
+import { v4 as uuidv4 } from 'uuid';
+import './Styling/Chat.css';
 
-const APP_ID = "bc38613fec1e4347a1c3e44c0473d06f";
-const CHANNEL_NAME = "abc";
+const APP_ID = 'bc38613fec1e4347a1c3e44c0473d06f';
+const CHANNEL = 'abc';
 
-const client = AgoraRTM.createInstance(APP_ID);
-const uid = uuid();
+let client = AgoraRTM.createInstance(APP_ID);
+let uid = uuidv4();
 
-function Chat() {
-  const [text, setText] = useState("");
-  const [channel, setChannel] = useState(null);
+import React, { useEffect, useRef, useState } from 'react';
+
+export default function Chat() {
+  const messagesRef = useRef();
   const [messages, setMessages] = useState([]);
+  const [text, setText] = useState('');
+  const [channel, setChannel] = useState(null);
+
+  const appendMessage = (message) => {
+    setMessages((messages) => [...messages, message]);
+  };
 
   useEffect(() => {
     const connect = async () => {
       await client.login({ uid, token: null });
-      const channel = await client.createChannel(CHANNEL_NAME);
+      const channel = await client.createChannel(CHANNEL);
       await channel.join();
-      channel.on("ChannelMessage", (message, memberId) => {
-        setMessages((currentMessages) => [
-          ...currentMessages,
-          {
-            uid: memberId,
-            text: message.text,
-          },
-        ]);
+      channel.on('ChannelMessage', (message, peerId) => {
+        appendMessage({
+          text: message.text,
+          uid: peerId,
+        });
       });
       setChannel(channel);
       return channel;
@@ -35,56 +37,63 @@ function Chat() {
     const connection = connect();
 
     return () => {
-      const logout = async () => {
+      const disconnect = async () => {
         const channel = await connection;
-        await client.logout();
         await channel.leave();
+        await client.logout();
       };
-      logout();
+      disconnect();
     };
   }, []);
 
+  useEffect(() => {
+    messagesRef.current.scrollTop =
+      messagesRef.current.scrollHeight;
+  }, [messages]);
+
   const sendMessage = (e) => {
     e.preventDefault();
-    if (text === "") return;
-    channel.sendMessage({
-      text,
-      type: "text",
+    if (text === '') return;
+    channel.sendMessage({ text, type: 'text' });
+    appendMessage({
+      text: text,
+      uid,
     });
-    setMessages((currentMessages) => [
-        ...currentMessages,
-        {
-          uid,
-          text
-        },
-    ]);
-    setText("");
+    setText('');
   };
 
   return (
     <main>
       <div className="panel">
-        <div className="messages">
+        <div className="messages" ref={messagesRef}>
           <div className="inner">
-            {messages.map((message, index) => (
-              <div key={index} className="message">
-                {message.uid === uid && <div className="user-self">You: &nbsp;</div>}
-                {message.uid !== uid && <div className="user-them">Them: &nbsp;</div>}
-                {message.text}
+            {messages.map((message, idx) => (
+              <div key={idx} className="message">
+                {message.uid === uid && (
+                  <div className="user-self">
+                    You:&nbsp;
+                  </div>
+                )}
+                {message.uid !== uid && (
+                  <div className="user-them">
+                    Them:&nbsp;
+                  </div>
+                )}
+                <div className="text">{message.text}</div>
               </div>
             ))}
           </div>
         </div>
+
+        <form onSubmit={sendMessage}>
+          <input
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button>+</button>
+        </form>
       </div>
-      <form onSubmit={sendMessage}>
-        <input
-          value={text}
-          onChange={(e) => setText(e.currentTarget.value)}
-        ></input>
-        <button>+</button>
-      </form>
     </main>
   );
 }
 
-export default Chat;
